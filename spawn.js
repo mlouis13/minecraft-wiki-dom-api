9b833c0bea225a51560d95bd145b267d599f46ab
sfacchino <sfacchino@edenschool.fr>
2026-05-13 11:44:58 +0200

    W update du js

--- spawn.js
+++ spawn.js
@@ -1,11 +1,13 @@
 const API_URL = "http://10.69.4.208:3000/v1/";
 const form = document.querySelector("#chooseMob");
 const select = document.querySelector("#arenachoose");
+const tableBody = document.querySelector("#entitiesTableBody");
 
 document.addEventListener("DOMContentLoaded", async () => {
 	const response = await fetch(API_URL + "entities");
 	const data = await response.json();
 	console.log(data);
+	searchmob();
 	for (let i = 0; i < data.length; i++) {
 		const neuille = document.createElement("option");
 		neuille.setAttribute("value", data[i].id);
@@ -30,7 +32,6 @@ async function spawnEntities(a, b, c) {
 		x: b,
 		z: c,
 	};
-	console.log(body);
 	const response = await fetch(API_URL + "arena/entities", {
 		method: "POST",
 		headers: {
@@ -41,4 +42,48 @@ async function spawnEntities(a, b, c) {
 
 	const data = await response.json();
 	console.log(data);
+}
+
+async function searchmob() {
+	const response = await fetch(API_URL + "arena/entities");
+
+	const data = await response.json();
+	for (let i = 0; i < data.length; i++) {
+		const tr = document.createElement("tr");
+		const tdImg = document.createElement("td");
+		const img = document.createElement("img");
+		img.setAttribute("src", data[i].entity.icon);
+		img.width = 40;
+
+		tdImg.appendChild(img);
+		const tdName = document.createElement("td");
+		tdName.textContent = data[i].entity.name;
+
+		const tdX = document.createElement("td");
+		tdX.textContent = data[i].arena.x;
+
+		const tdZ = document.createElement("td");
+		tdZ.textContent = data[i].arena.z;
+
+		const tdstrength = document.createElement("td");
+		tdstrength.textContent = data[i].entity.strength
+
+		
+		const button = document.createElement("button");
+
+		button.textContent = "DELETE";
+
+		
+		button.classList.add("btndel");
+
+		
+		tr.appendChild(tdImg);
+		tr.appendChild(tdName);
+		tr.appendChild(tdX);
+		tr.appendChild(tdZ);
+		tr.appendChild(tdstrength);
+		tr.appendChild(button);
+		tr.classList.add("tblbody");
+		tableBody.appendChild(tr);
+	}
 }

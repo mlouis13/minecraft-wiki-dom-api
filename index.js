00c22d503c69c21395494a95398fd3559dd2a939
mlouis13 <mlouis@edenschool.fr>
2026-05-13 19:27:45 +0200

    repo a ameliorer(vrm bcp)

--- index.js
+++ index.js
@@ -1,77 +1,99 @@
-const URL = "http://10.69.4.208:3000/v1/";
+const URL = "http://play.hatlas.net:3000/v1/";
 const form = document.querySelector("#searchEntity");
 const tabEntity = document.querySelector("#tabEntity");
 const remove = document.querySelector("#remove");
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const data = new FormData(form);
-	const name = data.get("name");
-	const classification = data.get("classification");
-	const type = data.get("type");
-	const health = data.get("health");
-	const armor = data.get("armor");
-	const damage = data.get("damage");
-	searchEntities(name, classification, type, health, armor, damage);
-	tabEntity.classList.remove("noentity");
-	tabEntity.classList.add("noentity2");
+  e.preventDefault();
+  const data = new FormData(form);
+  const name = data.get("name");
+  const classification = data.get("classification");
+  const type = data.get("type");
+  const health = data.get("health");
+  const armor = data.get("armor");
+  const damage = data.get("damage");
+  test(name, classification, type, health, armor, damage);
+  tabEntity.classList.remove("noentity");
+  tabEntity.classList.add("noentity2");
 });
-async function searchEntities(
-	name,
-	classification,
-	type,
-	health,
-	armor,
-	damage,
-) {
-	let response = await fetch(URL + "entities");
-	let responseJS = await response.json();
-	console.log(responseJS);
-	remove.remove();
+function test(name, classification, type, health, armor, damage) {
+  if (name !== "") {
+    searchEntities(URL + "entities?name=" + name);
+  } else {
+    if (classification !== "") {
+      searchEntities(URL + "entities?classification=" + classification);
+    } else {
+      if (type !== "") {
+        searchEntities(URL + "entities?type=" + type);
+      } else {
+        if (health !== "") {
+          searchEntities(URL + "entities?health=" + health);
+        } else {
+          if (armor !== "") {
+            searchEntities(URL + "entities?armor=" + armor);
+          } else {
+            if (damage !== "") {
+              searchEntities(URL + "entities?damage=" + damage);
+            } else {
+              searchEntities(URL + "entities");
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
-	for (let i = 0; i < responseJS.length; i++) {
-		const name1 = document.createElement("h2");
-		name1.classList.add("name");
-		name1.textContent = responseJS[i].name;
-		const img = document.createElement("img");
-		img.setAttribute("src", responseJS[i].image);
-		img.style.width = "210px";
-		img.style.height = "150px";
-		const div = document.createElement("div");
-		div.classList.add("align");
-		const classification1 = document.createElement("p");
-		classification1.textContent = responseJS[i].classification;
-		classification1.classList.add("fauxA");
-		const type1 = document.createElement("p");
-		type1.classList.add("type");
-		type1.textContent = responseJS[i].type;
-		const hr = document.createElement("hr");
-		hr.classList.add("hr");
-		const btn = document.createElement("button");
-		btn.textContent = "SEE MORE";
-		btn.classList.add("btn");
-		const card = document.createElement("div");
-		card.classList.add("div");
-		if (responseJS[i].type == "neutral") {
-			card.classList.add("divbeige");
-			card.classList.remove("div");
-			name1.classList.add("namebeige");
-			name1.classList.add("name");
-			hr.classList.add("hrbeige");
-			hr.classList.remove("hr");
-			btn.classList.add("btnbeige");
-			btn.classList.remove("btn");
-		} else if (responseJS[i].type == "hostile") {
-			card.classList.add("divred");
-			card.classList.remove("div");
-			name1.classList.add("namered");
-			name1.classList.add("name");
-			hr.classList.add("hrred");
-			hr.classList.remove("hr");
-			btn.classList.add("btnred");
-			btn.classList.remove("btn");
-		}
-		div.append(classification1, type1);
-		card.append(name1, img, div, hr, btn);
-		tabEntity.appendChild(card);
-	}
+async function searchEntities(info) {
+  console.log(info);
+  let response = await fetch(info);
+  let responseJS = await response.json();
+  console.log(responseJS);
+  remove.remove();
+
+  for (let i = 0; i < responseJS.length; i++) {
+    const name1 = document.createElement("h2");
+    name1.classList.add("name");
+    name1.textContent = responseJS[i].name;
+    const img = document.createElement("img");
+    img.setAttribute("src", responseJS[i].image);
+    img.style.width = "210px";
+    img.style.height = "150px";
+    const div = document.createElement("div");
+    div.classList.add("align");
+    const classification1 = document.createElement("p");
+    classification1.textContent = responseJS[i].classification;
+    classification1.classList.add("fauxA");
+    const type1 = document.createElement("p");
+    type1.classList.add("type");
+    type1.textContent = responseJS[i].type;
+    const hr = document.createElement("hr");
+    hr.classList.add("hr");
+    const btn = document.createElement("button");
+    btn.textContent = "SEE MORE";
+    btn.classList.add("btn");
+    const card = document.createElement("div");
+    card.classList.add("div");
+    if (responseJS[i].type == "neutral") {
+      card.classList.add("divbeige");
+      card.classList.remove("div");
+      name1.classList.add("namebeige");
+      name1.classList.add("name");
+      hr.classList.add("hrbeige");
+      hr.classList.remove("hr");
+      btn.classList.add("btnbeige");
+      btn.classList.remove("btn");
+    } else if (responseJS[i].type == "hostile") {
+      card.classList.add("divred");
+      card.classList.remove("div");
+      name1.classList.add("namered");
+      name1.classList.add("name");
+      hr.classList.add("hrred");
+      hr.classList.remove("hr");
+      btn.classList.add("btnred");
+      btn.classList.remove("btn");
+    }
+    div.append(classification1, type1);
+    card.append(name1, img, div, hr, btn);
+    tabEntity.appendChild(card);
+  }
 }
--- spawn.js
+++ spawn.js
@@ -1,89 +1,86 @@
-const API_URL = "http://10.69.4.208:3000/v1/";
+const API_URL = "http://play.hatlas.net:3000/v1/";
 const form = document.querySelector("#chooseMob");
 const select = document.querySelector("#arenachoose");
 const tableBody = document.querySelector("#entitiesTableBody");
 
 document.addEventListener("DOMContentLoaded", async () => {
-	const response = await fetch(API_URL + "entities");
-	const data = await response.json();
-	console.log(data);
-	searchmob();
-	for (let i = 0; i < data.length; i++) {
-		const neuille = document.createElement("option");
-		neuille.setAttribute("value", data[i].id);
-		neuille.textContent = data[i].name;
-		select.appendChild(neuille);
-	}
+  const response = await fetch(API_URL + "entities");
+  const data = await response.json();
+  console.log(data);
+  searchmob();
+  for (let i = 0; i < data.length; i++) {
+    const neuille = document.createElement("option");
+    neuille.setAttribute("value", data[i].id);
+    neuille.textContent = data[i].name;
+    select.appendChild(neuille);
+  }
 });
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const newdata = new FormData(form);
-	const entityId = parseInt(newdata.get("mob"));
-	const x = Number.parseInt(newdata.get("coX"));
-	const z = Number.parseInt(newdata.get("coZ"));
-	console.log(parseInt(entityId));
-	spawnEntities(entityId, x, z);
+  e.preventDefault();
+  const newdata = new FormData(form);
+  const entityId = parseInt(newdata.get("mob"));
+  const x = Number.parseInt(newdata.get("coX"));
+  const z = Number.parseInt(newdata.get("coZ"));
+  console.log(parseInt(entityId));
+  spawnEntities(entityId, x, z);
 });
 
 async function spawnEntities(a, b, c) {
-	const body = {
-		entityId: a,
-		x: b,
-		z: c,
-	};
-	const response = await fetch(API_URL + "arena/entities", {
-		method: "POST",
-		headers: {
-			"content-type": "application/json",
-		},
-		body: JSON.stringify(body),
-	});
+  const body = {
+    entityId: a,
+    x: b,
+    z: c,
+  };
+  const response = await fetch(API_URL + "arena/entities", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
 
-	const data = await response.json();
-	console.log(data);
+  const data = await response.json();
+  console.log(data);
 }
 
 async function searchmob() {
-	const response = await fetch(API_URL + "arena/entities");
+  const response = await fetch(API_URL + "arena/entities");
 
-	const data = await response.json();
-	for (let i = 0; i < data.length; i++) {
-		const tr = document.createElement("tr");
-		const tdImg = document.createElement("td");
-		const img = document.createElement("img");
-		img.setAttribute("src", data[i].entity.icon);
-		img.width = 40;
+  const data = await response.json();
+  for (let i = 0; i < data.length; i++) {
+    const tr = document.createElement("tr");
+    const tdImg = document.createElement("td");
+    const img = document.createElement("img");
+    img.setAttribute("src", data[i].entity.icon);
+    img.width = 40;
 
-		tdImg.appendChild(img);
-		const tdName = document.createElement("td");
-		tdName.textContent = data[i].entity.name;
+    tdImg.appendChild(img);
+    const tdName = document.createElement("td");
+    tdName.textContent = data[i].entity.name;
 
-		const tdX = document.createElement("td");
-		tdX.textContent = data[i].arena.x;
+    const tdX = document.createElement("td");
+    tdX.textContent = data[i].arena.x;
 
-		const tdZ = document.createElement("td");
-		tdZ.textContent = data[i].arena.z;
+    const tdZ = document.createElement("td");
+    tdZ.textContent = data[i].arena.z;
 
-		const tdstrength = document.createElement("td");
-		tdstrength.textContent = data[i].entity.strength
+    const tdstrength = document.createElement("td");
+    tdstrength.textContent = data[i].entity.strength;
 
-		
-		const button = document.createElement("button");
+    const button = document.createElement("button");
 
-		button.textContent = "DELETE";
+    button.textContent = "DELETE";
 
-		
-		button.classList.add("btndel");
+    button.classList.add("btndel");
 
-		
-		tr.appendChild(tdImg);
-		tr.appendChild(tdName);
-		tr.appendChild(tdX);
-		tr.appendChild(tdZ);
-		tr.appendChild(tdstrength);
-		tr.appendChild(button);
-		tr.classList.add("tblbody");
-		tableBody.appendChild(tr);
-	}
+    tr.appendChild(tdImg);
+    tr.appendChild(tdName);
+    tr.appendChild(tdX);
+    tr.appendChild(tdZ);
+    tr.appendChild(tdstrength);
+    tr.appendChild(button);
+    tr.classList.add("tblbody");
+    tableBody.appendChild(tr);
+  }
 }

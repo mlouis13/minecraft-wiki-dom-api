4489bb9dbbac955d4cf7f8ea4088cb283d1458fe
mlouis13 <mlouis@edenschool.fr>
2026-05-15 16:57:14 +0200

    repo 1

--- index.js
+++ index.js
@@ -56,118 +56,62 @@ async function searchEntities(info) {
   let responseJS = await response.json();
   console.log(responseJS);
   if (responseJS.length > 0) {
-    if (isClicked == false) {
-      tabEntity.innerHTML = "";
-      remove.style.display = "none";
-      tabEntity.classList.remove("noentity");
-      tabEntity.classList.add("noentity2");
-      for (let i = 0; i < responseJS.length; i++) {
-        const name1 = document.createElement("h2");
+    tabEntity.innerHTML = "";
+    remove.style.display = "none";
+    tabEntity.classList.remove("noentity");
+    tabEntity.classList.add("noentity2");
+    for (let i = 0; i < responseJS.length; i++) {
+      const name1 = document.createElement("h2");
+      name1.classList.add("name");
+      name1.textContent = responseJS[i].name;
+      const img = document.createElement("img");
+      img.setAttribute("src", responseJS[i].image);
+      img.style.width = "210px";
+      img.style.height = "150px";
+      const div = document.createElement("div");
+      div.classList.add("align");
+      const classification1 = document.createElement("p");
+      classification1.textContent = responseJS[i].classification;
+      classification1.classList.add("fauxA");
+      const type1 = document.createElement("p");
+      type1.classList.add("type");
+      type1.textContent = responseJS[i].type;
+      const hr = document.createElement("hr");
+      hr.classList.add("hr");
+      const btn = document.createElement("button");
+      btn.textContent = "SEE MORE";
+      btn.classList.add("btn");
+      const card = document.createElement("div");
+      card.classList.add("div");
+      if (responseJS[i].type == "neutral") {
+        card.classList.add("divbeige");
+        card.classList.remove("div");
+        name1.classList.add("namebeige");
         name1.classList.add("name");
-        name1.textContent = responseJS[i].name;
-        const img = document.createElement("img");
-        img.setAttribute("src", responseJS[i].image);
-        img.style.width = "210px";
-        img.style.height = "150px";
-        const div = document.createElement("div");
-        div.classList.add("align");
-        const classification1 = document.createElement("p");
-        classification1.textContent = responseJS[i].classification;
-        classification1.classList.add("fauxA");
-        const type1 = document.createElement("p");
-        type1.classList.add("type");
-        type1.textContent = responseJS[i].type;
-        const hr = document.createElement("hr");
-        hr.classList.add("hr");
-        const btn = document.createElement("button");
-        btn.textContent = "SEE MORE";
-        btn.classList.add("btn");
-        const card = document.createElement("div");
-        card.classList.add("div");
-        if (responseJS[i].type == "neutral") {
-          card.classList.add("divbeige");
-          card.classList.remove("div");
-          name1.classList.add("namebeige");
-          name1.classList.add("name");
-          hr.classList.add("hrbeige");
-          hr.classList.remove("hr");
-          btn.classList.add("btnbeige");
-          btn.classList.remove("btn");
-        } else if (responseJS[i].type == "hostile") {
-          card.classList.add("divred");
-          card.classList.remove("div");
-          name1.classList.add("namered");
-          name1.classList.add("name");
-          hr.classList.add("hrred");
-          hr.classList.remove("hr");
-          btn.classList.add("btnred");
-          btn.classList.remove("btn");
-        }
-        div.append(classification1, type1);
-        card.append(name1, img, div, hr, btn);
-        tabEntity.appendChild(card);
-        btn.addEventListener("click", () => {
-          window.location.href = "details.html?name=" + responseJS[i].name;
-          console.log(responseJS[i]);
-        });
-        isClicked = true;
-      }
-    } else {
-      tabEntity.innerHTML = "";
-      remove.style.display = "none";
-      tabEntity.classList.remove("noentity");
-      tabEntity.classList.add("noentity2");
-      for (let i = 0; i < responseJS.length; i++) {
-        const name1 = document.createElement("h2");
+        hr.classList.add("hrbeige");
+        hr.classList.remove("hr");
+        btn.classList.add("btnbeige");
+        btn.classList.remove("btn");
+      } else if (responseJS[i].type == "hostile") {
+        card.classList.add("divred");
+        card.classList.remove("div");
+        name1.classList.add("namered");
         name1.classList.add("name");
-        name1.textContent = responseJS[i].name;
-        const img = document.createElement("img");
-        img.setAttribute("src", responseJS[i].image);
-        img.style.width = "210px";
-        img.style.height = "150px";
-        const div = document.createElement("div");
-        div.classList.add("align");
-        const classification1 = document.createElement("p");
-        classification1.textContent = responseJS[i].classification;
-        classification1.classList.add("fauxA");
-        const type1 = document.createElement("p");
-        type1.classList.add("type");
-        type1.textContent = responseJS[i].type;
-        const hr = document.createElement("hr");
-        hr.classList.add("hr");
-        const btn = document.createElement("button");
-        btn.textContent = "SEE MORE";
-        btn.classList.add("btn");
-        const card = document.createElement("div");
-        card.classList.add("div");
-        if (responseJS[i].type == "neutral") {
-          card.classList.add("divbeige");
-          card.classList.remove("div");
-          name1.classList.add("namebeige");
-          name1.classList.add("name");
-          hr.classList.add("hrbeige");
-          hr.classList.remove("hr");
-          btn.classList.add("btnbeige");
-          btn.classList.remove("btn");
-        } else if (responseJS[i].type == "hostile") {
-          card.classList.add("divred");
-          card.classList.remove("div");
-          name1.classList.add("namered");
-          name1.classList.add("name");
-          hr.classList.add("hrred");
-          hr.classList.remove("hr");
-          btn.classList.add("btnred");
-          btn.classList.remove("btn");
-        }
-        div.append(classification1, type1);
-        card.append(name1, img, div, hr, btn);
-        tabEntity.appendChild(card);
-        btn.addEventListener("click", () => {
-          window.location.href = "details.html?name=" + responseJS[i].name;
-          console.log(responseJS[i]);
-        });
-        isClicked = false;
+        hr.classList.add("hrred");
+        hr.classList.remove("hr");
+        btn.classList.add("btnred");
+        btn.classList.remove("btn");
       }
+      div.append(classification1, type1);
+      card.append(name1, img, div, hr, btn);
+      tabEntity.appendChild(card);
+      btn.addEventListener("click", () => {
+        window.location.href = "details.html?name=" + responseJS[i].name;
+        console.log(responseJS[i]);
+      });
     }
+  } else {
+    remove.style.display = "flex";
+    console.log("lol");
   }
 }
